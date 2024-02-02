bf502d29c94dafdb78e4951745bbeac1ecf27fb6
John Francis <john9francis@gmail.com>
2024-02-02 14:09:23 -0700

    Added methods to calculate the external forces on the planet based on position and distance

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from pygame import Vector2
 from planet import Planet
 
 def main():
 
+  sun_mass = 100
+  sun_pos = Vector2(0,1.5)
+
   p = Planet()
+  p.add_external_force(sun_pos, sun_mass)
+  print(p.total_force)
   pass
 
 
--- planet.py
+++ planet.py
@@ -14,25 +14,47 @@ class Planet:
   total_force = Vector2(0,0)
 
   def __init__(self) -> None:
-    print("Hello Planet World!")
-
-
-  def add_force_vector(self, other_pos, other_mass) -> None:
-    '''
-    Takes in another planet's position and mass,
-    and calculates the force on this planet based on that.
-    It adds this force vector to the total force
-    '''
     pass
 
 
-  def gravitational_force(self, m_other, r_magnitude):
+  def add_external_force(self, other_pos:Vector2, other_mass:float) -> None:
+    '''
+    Takes in another planet's position and mass,
+    and calculates the force on this planet based on that.
+    It adds this force vector to this planets total force
+    '''
+
+    # first, get the force as a scalar
+    r = self.find_r(other_pos)
+    r_mag = r.length()
+    force = self.gravitational_force(other_mass, r_mag)
+
+    # second, add the force to the total force variable
+    direction = r.normalize()
+    self.total_force += direction * force
+    pass
+
+
+
+  def reset_external_forces(self):
+    self.total_force = Vector2(0,0)
+
+
+  def gravitational_force(self, m_other:float, r_magnitude:float)->float:
     '''
     Calculates the gravitational force scalar 
     based on the equation:
     F = G*m1*m2/r**2, 
-    returns F.
+    returns F/m so can be used as acceleration.
     '''
-    return self.G * self.mass * m_other / (r_magnitude**2)
+    return self.G * m_other / (r_magnitude**2)
+  
+  
+  def find_r(self, pos_other:Vector2) -> Vector2:
+    '''
+    Takes in a pos vector of the other object
+    and returns a vector FROM this object TO the other
+    '''
+    return pos_other - self.pos
 
   pass
