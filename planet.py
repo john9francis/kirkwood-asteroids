c7e2dfc050b32067b081bd4a42775dc32c6ce0bf
John Francis <john9francis@gmail.com>
2024-02-02 13:50:17 -0700

    setup the gravitational force function that calculates the magnitude of the force

--- planet.py
+++ planet.py
@@ -1,5 +1,17 @@
+from pygame import Vector2
+
 class Planet:
   '''extends stationary planet but adds movement'''
+
+  # Constants
+  G = 6.6743 * 10**(-11) # m^3 kg^-1 s^-2
+
+  # member variables
+  mass = 1
+  pos = Vector2(0,0)
+
+  # changeable variables
+  total_force = Vector2(0,0)
 
   def __init__(self) -> None:
     print("Hello Planet World!")
@@ -14,9 +26,13 @@ class Planet:
     pass
 
 
-  def gravitational_force(self, m_other, r_other):
+  def gravitational_force(self, m_other, r_magnitude):
     '''
-    Calculates the gravitational force based on the equation
-    F = G*m1*m2/r**2, returns F.
+    Calculates the gravitational force scalar 
+    based on the equation:
+    F = G*m1*m2/r**2, 
+    returns F.
     '''
+    return self.G * self.mass * m_other / (r_magnitude**2)
+
   pass
