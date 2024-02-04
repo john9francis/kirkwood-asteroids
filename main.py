7b291829bd7eeb05d12fd021b578a6e44ff2be18
John Francis <john9francis@gmail.com>
2024-02-03 21:12:07 -0700

    Got some kind of orbit going, but again I need to figure out units.

--- main.py
+++ main.py
@@ -13,21 +13,21 @@ def main():
 
   # Initial conditions
   dt = 0.1
-  r_initial = np.array([-10000000000, 0])
-  v_initial = np.array([0, 10000000000])
+  r_initial = np.array([103000000, 0])
+  v_initial = np.array([1, 1])
   t = [0]
   r = [r_initial]
   v = [v_initial]
 
   # set the planet's force values
-  p.set_pos(r[-1])
-  p.reset_external_forces()
-  p.add_external_force(sun_pos, sun_mass)
+  #p.set_pos(r[-1])
+  #p.reset_external_forces()
+  #p.add_external_force(sun_pos, sun_mass)
 
   # start with a little euler's method to fill the h + .5 value
-  v.append(v[-1] + p.get_total_force() * .5 * dt)
-  r.append(r[-1] + r[-1] * 0.5 * dt)
-  t.append(t[-1] + 0.5 * dt)
+  #v.append(v[-1] + p.get_total_force() * .5 * dt)
+  #r.append(r[-1] + v[-1] * 0.5 * dt)
+  #t.append(t[-1] + 0.5 * dt)
 
   # now we have 2 data points, we start leap frogging
   while t[-1] < 100:
@@ -40,14 +40,22 @@ def main():
     # acceleration is the total force
     a = p.get_total_force()
 
-    # leapfrog to find new v
-    new_v = v[-2] + a * v[-1] * dt
+    v_half = v[-1] + a * .5 * dt
 
-    # leapfrog to find new r
-    new_r = r[-2] + dt * new_v * r[-1]
+    new_r = r[-1] + v_half * dt
+
+    # reset the planet to get the new force in there
+    p.set_pos(r[-1])
+    p.reset_external_forces()
+    p.add_external_force(sun_pos, sun_mass)
+
+    # acceleration is the total force
+    a = p.get_total_force()
+
+    new_v = v_half + a * .5 * dt
 
     # add variables to the lists
-    t.append(t[-1] + 0.5 * dt)
+    t.append(t[-1] + dt)
     v.append(new_v)
     r.append(new_r)
 
