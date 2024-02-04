db054e20f390636fe7da8aa5f42f411d28ec816e
John Francis <john9francis@gmail.com>
2024-02-03 19:43:13 -0700

    created a new class that I may or may not use

---
+++ leapfrog.py
@@ -0,0 +1,13 @@
+from planet import Planet
+
+class Leapfrog:
+  '''
+  A class that performs the leapfrog method
+  to calculate the next positions and velocities
+  of several planets
+  '''
+
+  planet_list = []
+
+  def __init__(self) -> None:
+    pass
